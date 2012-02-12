2929ef7fc95d4ca169936a591fb792de4fb7b1b7
Joakim Lidström <joakim@kurppahosk.com>
2012-02-12 21:57:38 +0100

    Removed too much

---
+++ content.php
@@ -0,0 +1,22 @@
+<section id="gridCanvas" style="height: 1560px; "><div id="block_1"  class="grid-block"><h1>Tweetica</h1>
+
+<p>Based on <strong>Twitter's API</strong></p></div><div id="block_2"  class="grid-block"><p><img src="/images/tweetica-hero.png" alt="Tweetica!" title="" /></p></div><div id="block_3"  class="grid-block"><ul>
+<li>CON</li>
+<li>UX/UI</li>
+<li>DEV</li>
+</ul></div><div id="block_4"  class="grid-block"><p class="preamble">
+
+For fans of Warp Records, Timothy Saccenti’s imagery should be well known. The iconic LA-based photographer / director has worked with some of the label’s most popular artists like Battles (the Mirrored artwork being arguably one of the key images of 2007) and more recently the unstoppable Flying Lotus.
+
+</p></div><div id="block_5"  class="grid-block"><p>Tweetica fetches live tweets as a screensaver.</p></div><div id="block_6"  class="grid-block"><p><img src="/images/tweetica-screenshot.png" alt="Tweetica Screenshot" title="" /></p></div><div id="block_7"  class="grid-block"><p>For fans of Warp Records, Timothy Saccenti’s imagery should be well known. The iconic LA-based photographer / director has worked with some of the label’s most popular artists like Battles (the Mirrored artwork being arguably one of the key images of 2007) and more recently the unstoppable Flying Lotus.</p>
+
+<p>Being a self-professed Warp fanboy meant that working with Tim was an opportunity not to be missed, especially with Build directing. The design plans detailed a site that until recently might only have been attempted with Flash, but it being 2010 (and the iPad just around the corner) we set out to push the boundaries of HTML.</p>
+
+<p>Cufon type substitution, custom slide/reel mechanics (including mousewheel control), a bespoke media player and fullscreen image viewer all added up to a thoroughly modern website. If you find it a little uncompromising well this was mostly the Antenna's doing - Tim trusted us to go beyond a ’safe‘ experience, is anything.</p></div><div id="block_8"  class="grid-block"><p><img src="/images/screens.png" alt="Hello" title="" /></p></div><div id="block_9"  class="grid-block"><p><strong>1+ SCR</strong></p></div><div id="block_10"  class="grid-block"><p>Substitution, custom slide/reel mechanics (including mousewheel control), a bespoke media player and fullscreen image viewer all added up to a thoroughly modern website. If you find it a little.</p>
+
+<p>Fullscreen image viewer all added up to a thoroughly modern website.</p></div><div id="block_11"  class="grid-block"><h4>Tags</h4>
+
+<p>Being a self-professed <strong>Warp</strong> fanboy meant that working with Tim was an opportunity not to be missed, especially with Build directing. The design plans detailed a site that until recently might only have been attempted with Flash. </p>
+
+<p>But it being 2010 (and the iPad just around the corner) we set out to push the boundaries of HTML.</p></div><div id="block_12"  class="grid-block"><p><img src="/images/tweetica-settings.png" alt="" title="" /></p></div><div id="block_13"  class="grid-block"><p><strong>10.6</strong> <br>
+<strong>10.7</strong></p></div><div id="block_14"  class="grid-block"><p><img src="/images/tweetica-screens.png" alt="Tweetica Screens" title="" /></p></div></section>
